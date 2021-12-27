52819c811d3bc65f520ae34fe930b42bf5943050
william.liao <william.liao@mitake.com.tw>
2021-12-27 16:16:36 +0800

    Initial Commit

---
+++ HelloGitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloGitHub
+//
+//  Created by 雲端開發部-廖彥勛 on 2021/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
