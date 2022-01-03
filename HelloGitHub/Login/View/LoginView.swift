89104e27aa73030c69c269d240d0ec06b9a3f312
william.liao <william.liao@mitake.com.tw>
2022-01-03 10:50:10 +0800

    update LoginView

--- HelloGitHub/Login/View/LoginView.swift
+++ HelloGitHub/Login/View/LoginView.swift
@@ -9,71 +9,28 @@ import UIKit
 
 class LoginView: UIView {
 
-    private lazy var userAccountTextField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        textField.textColor = .label
-        textField.font = UIFont.systemFont(ofSize: 12)
-        textField.textAlignment = .left
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
-    }()
-    
-    private lazy var userPasswordTextField: UITextField = {
-        let textField = UITextField()
-        textField.borderStyle = .roundedRect
-        textField.textColor = .label
-        textField.font = UIFont.systemFont(ofSize: 12)
-        textField.textAlignment = .left
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
-    }()
-    
     private lazy var loginButton: UIButton = {
         let button = UIButton(type: .custom)
         button.setTitle("Login", for: .normal)
         button.setTitleColor(.label, for: .normal)
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = button.frame.height/2
         button.addTarget(self, action: #selector(onTapLogin), for: .touchUpInside)
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
     
     func configureView() {
-        self.addSubview(userAccountTextField)
-        self.addSubview(userPasswordTextField)
         self.addSubview(loginButton)
     }
     
     func configureConstraints() {
         NSLayoutConstraint.activate([
-            
-            userAccountTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
-            userAccountTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
-            userAccountTextField.topAnchor.constraint(equalTo: self.topAnchor, constant: 22),
-            userAccountTextField.heightAnchor.constraint(equalToConstant: 44),
-            
-            userPasswordTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15),
-            userPasswordTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -15),
-            userPasswordTextField.topAnchor.constraint(equalTo: userAccountTextField.bottomAnchor, constant: 5),
-            userPasswordTextField.heightAnchor.constraint(equalToConstant: 44),
-
-            loginButton.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            loginButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 8),
+            loginButton.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -8),
+            loginButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -22),
             loginButton.heightAnchor.constraint(equalToConstant: 44),
         ])
-        
-        if #available(iOS 15.0, *) {
-            let buttonTop = self.keyboardLayoutGuide.topAnchor.constraint(equalToSystemSpacingBelow: loginButton.bottomAnchor, multiplier: 1.0)
-            buttonTop.identifier = "buttonTop"
-            self.keyboardLayoutGuide.setConstraints([buttonTop], activeWhenAwayFrom: .top)
-
-            let buttonBottom = loginButton.topAnchor.constraint(equalToSystemSpacingBelow: self.keyboardLayoutGuide.bottomAnchor, multiplier: 1.0)
-            buttonBottom.identifier = "buttonBottom"
-            self.keyboardLayoutGuide.setConstraints([buttonBottom], activeWhenNearEdge: .top)
-            
-        } else {
-            let buttonTop = loginButton.topAnchor.constraint(equalTo: userPasswordTextField.bottomAnchor, constant: 5)
-            NSLayoutConstraint.activate([buttonTop])
-        }
     }
     
     var viewModel: LoginViewModel
@@ -89,6 +46,6 @@ class LoginView: UIView {
     }
     
     @objc private func onTapLogin() {
-    
+        viewModel.userDidTapLogin()
     }
 }
--- HelloGitHub/Login/ViewModel/LoginViewModel.swift
+++ HelloGitHub/Login/ViewModel/LoginViewModel.swift
@@ -9,4 +9,8 @@ import Foundation
 
 class LoginViewModel {
     
+    
+    func userDidTapLogin() {
+        
+    }
 }
